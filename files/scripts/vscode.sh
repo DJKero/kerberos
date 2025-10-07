7a0c3f739eebc8669a531cc954c17c527012bf69
Ignacio Conde <djkero@proton.me>
2025-10-07 12:31:29 -0300

    Add packages and flatpaks

---
+++ files/scripts/vscode.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+echo 'Importing Microsoft GPG key and enabling VS Code repository'
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+sudo dnf check-update
